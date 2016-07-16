8cbec8e32455ca1c75297d14a666e693585402f4
Jethro Beekman <jethro@jbeekman.nl>
2016-07-16 15:43:46 -0700

    Add test for #212

--- tests/test_func.rs
+++ tests/test_func.rs
@@ -2,13 +2,41 @@ use support::assert_bind_eq;
 
 #[test]
 fn func_ptr() {
-    assert_bind_eq(Default::default(), "headers/func_ptr.h", "
-        extern \"C\" {
-            pub static mut foo: ::std::option::Option<
-                extern \"C\" fn(x: ::std::os::raw::c_int,
-                              y: ::std::os::raw::c_int) -> ::std::os::raw::c_int>;
+    assert_bind_eq(Default::default(), "headers/func_ptr.h", r#"
+        pub type fn_t = ::std::option::Option<extern "C" fn()>;
+        pub type fnptr_t = ::std::option::Option<extern "C" fn()>;
+        pub type fntptr_t = fn_t;
+        pub type fnptrptr_t = *mut ::std::option::Option<extern "C" fn()>;
+        pub type fntptrptr_t = *mut fntptr_t;
+        pub type fntptrtptr_t = *mut fn_t;
+        extern "C" {
+            pub static mut foo:
+                       ::std::option::Option<extern "C" fn(x: ::std::os::raw::c_int,
+                                                           y: ::std::os::raw::c_int)
+                                                 -> ::std::os::raw::c_int>;
         }
-    ");
+        extern "C" {
+            pub fn takes_argdef(proc_: ::std::option::Option<extern "C" fn()>);
+            pub fn takes_argdef_ptr(proc_: ::std::option::Option<extern "C" fn()>);
+            pub fn takes_argdef_ptr_ptr(proc_:
+                                            *mut ::std::option::Option<extern "C" fn()>);
+            pub fn takes_tdef(proc_: fn_t);
+            pub fn takes_tdef_ptr(proc_: fn_t);
+            pub fn takes_tdef_ptr_ptr(proc_: *mut fn_t);
+            pub fn takes_tdefptr(proc_: fnptr_t);
+            pub fn takes_tdefptr_ptr(proc_: *mut fnptr_t);
+            pub fn takes_tdefptr_ptr_ptr(proc_: *mut *mut fnptr_t);
+            pub fn takes_tdeftptr(proc_: fntptr_t);
+            pub fn takes_tdeftptr_ptr(proc_: *mut fntptr_t);
+            pub fn takes_tdeftptr_ptr_ptr(proc_: *mut *mut fntptr_t);
+            pub fn takes_tdefptrptr(proc_: fnptrptr_t);
+            pub fn takes_tdefptrptr_ptr(proc_: *mut fnptrptr_t);
+            pub fn takes_tdefptrptr_ptr_ptr(proc_: *mut *mut fnptrptr_t);
+            pub fn takes_tdeftptrtptr(proc_: fntptrtptr_t);
+            pub fn takes_tdeftptrtptr_ptr(proc_: *mut fntptrtptr_t);
+            pub fn takes_tdeftptrtptr_ptr_ptr(proc_: *mut *mut fntptrtptr_t);
+        }
+    "#);
 }
 
 #[test]
